38d6921518944fb0129343aef849a07323c91e66
jovannajdovski <j.najdovski@gmail.com>
2022-12-03 21:02:07 +0100

    Login component added

--- ihor/src/app/app.module.ts
+++ ihor/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from 'src/infrastructure/material.module';
+import { AppRoutingModule } from '../infrastructure/app-routing.module';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    // FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
