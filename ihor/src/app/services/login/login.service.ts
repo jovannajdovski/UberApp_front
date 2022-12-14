b3f0c342e6bdb03be657e87bdb65f0591f7cb85c
serbedzijajovan <jovajoksa@gmail.com>
2022-12-15 00:25:06 +0100

    Implemented login and register

--- ihor/src/app/services/login/login.service.ts
+++ ihor/src/app/services/login/login.service.ts
@@ -1,23 +1,63 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Login } from 'src/app/components/login/login.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  user$ = new BehaviorSubject(null);
+  userState$ = this.user$.asObservable();
+
   constructor(private http: HttpClient) { }
   
-  getUser(email: string, password: string): Observable<Tokens> {
+  login(auth: Login): Observable<Token> {
+    return this.http.post<Token>(environment.apiHost + "user/login",
+    {
+      email: auth.email, 
+      password: auth.password
+    })
+  }
+
+  logout(email: string, password: string): Observable<Token> {
+    return this.http.post<Token>(environment.apiHost + "user/logout",
+    {
+      email: email, 
+      password: password
+    })
+  }
+
+  setUser(): void {
+    this.user$.next(this.getRole());
+  }
+
+  getRole(): any {
+    if (this.isLoggedIn()) {
+      const accessToken: any = localStorage.getItem('user');
+      const helper = new JwtHelperService();
+      const role = helper.decodeToken(accessToken).role;
+      return role;
+    }
     
-    return this.http.post<Tokens>(environment.apiHost+"user/login",{email: email, password: password})
+    return null;
+  }
+
+  isLoggedIn(): boolean {
+    if (localStorage.getItem('user') != null)
+      return true;
+
+    return false;
   }
 }
 
-interface Tokens{
-  accessToken: string;
-  refreshToken: string;
+interface Token{
+  jwt: string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
-
