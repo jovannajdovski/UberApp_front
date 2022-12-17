bb1bc4fc484910be00999e9b5f5685de7d271171
Nikola1709 <savickosovska@gmail.com>
2022-12-17 18:04:24 +0100

    Started implementation of notification box

--- ihor/src/app/components/notifications/notifications.component.ts
+++ ihor/src/app/components/notifications/notifications.component.ts
@@ -1,3 +1,4 @@
+import { R3Identifiers } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,44 @@ import { Component } from '@angular/core';
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent {
 
+export class NotificationsComponent {
+  private adresa1="";
+  private adresa2="";
+  
+  public notifications: { ride: Ride, timestamp: string, type: NotificationType }[]
+  public notificationType=NotificationType;
+  constructor(){
+    const ride1: Ride={address1: 'Preradoviceva 40', address2: 'Futoska 50', time: '20:30', cash: 500, rejectionReason:'Umro mu pas'};
+    const ride2: Ride={address1: 'Laze Teleckog 2', address2: 'Bulevar oslobodjenja 100', time: '22:30', cash: 1500, rejectionReason:''};
+    this.notifications= [
+      {
+        ride: ride1,
+        timestamp: '22:50',
+        type: NotificationType.NEW_RIDE
+      },
+      {
+        ride: ride1,
+        timestamp: '18:10',
+        type: NotificationType.RIDE_REJECTION
+      },
+      {
+        ride: ride2,
+        timestamp: '17:40',
+        type: NotificationType.REMINDER
+      },
+    ];
+  }
+  
+}
+
+interface Ride {
+	address1: string;
+	address2: string;
+	time: string;
+	cash: number;
+  rejectionReason: string;
+}
+enum NotificationType {
+  NEW_RIDE, RIDE_REJECTION, REMINDER
 }
